da306b4dbb4425a17db92cc2e30ae5b991baff87
janobboy <qulamatovjanob@gmail.com>
2023-11-17 10:16:26 +0500

    code

--- While01.py
+++ While01.py
@@ -1,9 +1,11 @@
 def main(s):
-    """
-    A variable of type str is given. Find how many digits it contains and return.
-    Args:
-        s: str
-    Returns:
-        int: return answer
-    """
-    return
+    k = 0
+    i = 0
+    while i < len(s):
+        if s[i].isdigit():
+            k += 1
+        i += 1
+    return k
+
+print(main("python 2022"))
+print(main("e324xE"))
